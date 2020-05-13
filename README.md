6c021d44b043119916bb1f274beac59587c96a95
= <=>
2020-05-13 16:21:13 +0200

    Appended the GUI to the lczero branch

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # lichess.py
 yeah I cheat at chess, no I don't care. Program just parses the FEN from a lichess game and then pipes it into lczero.
- #### make sure to insert lczero in the default /$HOME/.local/bin/
+#### make sure to build lc0 in the directory: ~.local/bin
 
 
 ## Requirements:
+```
+pip3 install --user pyqt5  
+sudo apt-get install python3-pyqt5  
+sudo apt-get install pyqt5-dev-tools
+sudo apt-get install qttools5-dev-tools
+```
+```
+# apt-get install qt5-default libqt5webkit5-dev build-essential \
+                  python-lxml python-pip xvfb pip3 install dryscrape
+```
 [BeautifulSoup](https://pypi.org/project/bs4)
 
 [python-chess](https://pypi.org/project/python-chess)
--- scrape_fen.py
+++ scrape_fen.py
@@ -1,54 +1,128 @@
 #!/usr/bin/python3.8
+from PyQt5.QtWidgets import QLabel, QMainWindow, QDesktopWidget, QApplication
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import Qt, QTimer
 from bs4 import BeautifulSoup
+from multiprocessing import Process
 import chess.engine
+import chess.svg
 import chess
+import time
+import sys
 import re
 import os
 import logging
 import asyncio
 import dryscrape
 
-async def processing(FEN): # Parses FEN into stockfish/engine of choice
+
+class Incubator(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.initUI()
+
+    def initUI(self):
+        self.setFixedSize(700, 700)
+        self.centering()
+        self.content()
+        self.show()
+
+    def centering(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def content(self):
+        self.setWindowTitle('Chess Board Representation')
+        pixmap = QPixmap("pos.svg")
+        label = QLabel(self)
+        label.setPixmap(pixmap)
+        label.setAlignment(Qt.AlignCenter)
+        self.setCentralWidget(label)
+
+
+async def processing(FEN):  # Parses FEN into stockfish/engine of choice
     transport, engine = await chess.engine.popen_uci(f"{os.environ['HOME']}/.local/bin/lc0") 
-    
+
     ############################## Settings start
     await engine.configure({"RamLimitMb": 4096})
     await engine.configure({"Threads": 10})
     ############################## Settings end
-    
+
     board = chess.Board(FEN)
-    try:
-        result = await engine.play(board, chess.engine.Limit(time=5))
-    except:
-        print("+----------------| game finished |----------------+")
-        exit()
+    result = await engine.play(board, chess.engine.Limit(time=3))
     result = re.search(r"\((.*)\)>", str(result))
     result = re.sub('info={}, ', '', result.group(1))
     await engine.quit()
 
-    print(board)
-    print(result)
+    if board.is_stalemate() or board.is_game_over() or board.is_insufficient_material() is True:
+        print("+----------------| game finished |----------------+")
+        exit()
 
-def grabber(): # sends GET request to lichess & filters out FEN
+    move_from = result[5:7].upper()
+    move_to = result[7:9].upper()
+    SQUARES = [
+        "A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1",
+        "A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2",
+        "A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3",
+        "A4", "B4", "C4", "D4", "E4", "F4", "G4", "H4",
+        "A5", "B5", "C5", "D5", "E5", "F5", "G5", "H5",
+        "A6", "B6", "C6", "D6", "E6", "F6", "G6", "H6",
+        "A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7",
+        "A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8",
+    ]
+
+    itr = 0
+    for square in SQUARES:
+        if move_from == square:
+            move_from = itr
+        if move_to == square:
+            move_to = itr
+        itr += 1
+
+    flipping = None
+    sides = re.search(r'\s(w|b)\s', FEN)
+    if sides.group(1) == 'b':
+        flipping = True
+    else:
+        flipping = False
+    with open(f"pos.svg", 'w') as f:
+        f.write(chess.svg.board(board=board, size=700, flipped=flipping, arrows=[(move_from, move_to)]))
+
+
+def grabber():  # sends GET request to lichess & filters out FEN
     session.visit(url)
     soup = BeautifulSoup(session.body(), "lxml")
-    script = soup.body.findAll('script') 
+    script = soup.body.findAll('script')
     FEN = re.search('fen\":\"(.*)\",\"player\":', str(script))
     return FEN.group(1)
 
+
+def runtime():
+    FEN = None
+    while True:
+        check = grabber()
+        sides = re.search(r'\s(w|b)\s', check)
+        if check != FEN and sides.group(1) == clr:  # Checks whether an update in the chessboard occurred
+            asyncio.run(processing(check))
+            logging.debug(f'Inverted  FEN: {check}')
+        FEN = check
+
+
 if __name__ == "__main__":
-    logging.basicConfig(filename='output.log',level=logging.DEBUG)
+    app = QApplication(sys.argv)
+    logging.basicConfig(filename='output.log', level=logging.DEBUG)
     asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
     dryscrape.start_xvfb()
     session = dryscrape.Session()
     url = input("Enter lichess URL: ")
     clr = input("Enter playing side (b or w): ")
-    FEN = ""
-
-    while True: 
-        check = grabber()
-        sides = re.search(r'\s(w|b)\s', check)
-        if check != FEN and sides.group(1) == clr: # Checks whether an update in the chessboard occurred
-            asyncio.run(processing(check))
-            logging.debug(f'Inverted  FEN: {check}')
-        FEN = check
+    p1 = Process(target=runtime, daemon=True)
+    p1.start()
+    time.sleep(2)
+    win = Incubator()
+    timer = QTimer()
+    timer.start(500)
+    timer.timeout.connect(win.content)
+    sys.exit(app.exec_())
